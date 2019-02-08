c7d27847c42712b15651f8fce2a5cd8eed4739b0
Shanvy <Shanvy@192.168.0.9>
2019-02-08 08:28:53 +0530

    added sysout

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * 
+ * @author Shanvy
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("starting spring booot@@@@@@@@@@@");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
